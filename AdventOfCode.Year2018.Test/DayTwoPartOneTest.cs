d3f08a4f0446eb52b27f5deedfc8a63848c1cefd
Jelle de Jong <jelle@hoest.nl>
2018-12-02 15:23:06 +0100

    Day 2 - part 1 added

--- AdventOfCode.Year2018.Test/DayTwoPartOneTest.cs
+++ AdventOfCode.Year2018.Test/DayTwoPartOneTest.cs
@@ -1,5 +1,37 @@
 namespace AdventOfCode.Year2018.Test {
+  using System.IO;
+  using System.Linq;
+
+  using Xunit;
+
   public class DayTwoPartOneTest {
-    
+    [Fact]
+    public void Solution() {
+      const int Expected = 7657;
+
+      var input = File.ReadAllLines(
+          "/Users/jelle/Development/Hoest/AdventOfCode/AdventOfCode.Year2018.Test/Resources/InputD2P1.txt")
+        .ToArray();
+      var actual = DayTwo.Checksum(input);
+
+      Assert.Equal(Expected, actual);
+    }
+
+    [Fact]
+    public void Test1() {
+      var input = new[] {
+        "abcdef",
+        "bababc",
+        "abbcde",
+        "abcccd",
+        "aabcdd",
+        "abcdee",
+        "ababab"
+      };
+
+      const int Expected = 12;
+      var actual = DayTwo.Checksum(input);
+      Assert.Equal(Expected, actual);
+    }
   }
 }
--- AdventOfCode.Year2018/DayTwo.cs
+++ AdventOfCode.Year2018/DayTwo.cs
@@ -1,5 +1,19 @@
 namespace AdventOfCode.Year2018 {
+  using System.Collections.Generic;
+  using System.Linq;
+
   public class DayTwo {
-    
+    public static int Checksum(IEnumerable<string> input) {
+      var appearsTwice = Appearance(input, 2);
+      var appearsThreeTimes = Appearance(input, 3);
+
+      return appearsTwice * appearsThreeTimes;
+    }
+
+    private static int Appearance(IEnumerable<string> input, int number) {
+      return input
+        .Select(id => id.ToCharArray().GroupBy(c => c).Select(c => (character: c.Key, count: c.Count())).ToArray())
+        .Aggregate(0, (current, tuples) => current + (tuples.Any(c => c.count == number) ? 1 : 0));
+    }
   }
 }
